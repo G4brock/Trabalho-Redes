a96f29dd8733aac75a169282b27cdeedad4a6db7
Gabriel Rocha <gabriel.gabrielroch@gmail.com>
2021-12-03 10:37:06 -0300

    Implementado: operação de atuadores no cliente.

--- src/estufa/cliente.py
+++ src/estufa/cliente.py
@@ -1,7 +1,9 @@
 import json
 import socket
+import requests
 
 nome_limites = ["MAX_TEMP", "MIN_TEMP", "MAX_UMID", "MIN_UMID", "MAX_CO2", "MIN_CO2"]
+tipo_atuadores = ["irrigacao", "aquecedor", "injetorco2", "resfriador"]
 
 
 def cliente():
@@ -19,7 +21,7 @@ def cliente():
 
     # menu
     while True:
-        print("\n\nO que deseja fazer? (conf, ler, sair)")
+        print("\n\nO que deseja fazer? (conf, ler, oper, sair)")
         resposta = input()
 
         # configuar
@@ -41,6 +43,20 @@ def cliente():
                 r = json.loads(response[1])
                 print("Leituras:\t", r["leituras"])
                 print("Limites:\t", r["limites"])
+
+        if resposta == "oper":
+            print("\tTipos: aquecedor, resfriador, injetorco2 ou irrigacao.")
+            print("\tEstado: true ou false\n")
+            
+            atuador = input("Qual atuador você deseja operar? ")
+            estado = input("Qual o estado que você deseja colocar esse atuador?")
+            tipo = input("Qual o tipo de atuador? ")
+
+            if (estado.lower() == "true" or "false") and (tipo in tipo_atuadores):
+                response = requests.post(f"http://127.0.0.1:3000/{atuador}/{estado}/{tipo}")
+                print(response)
+            else:
+                print(f"valor inválido")
 
         if resposta == "sair":
             raise Exception
